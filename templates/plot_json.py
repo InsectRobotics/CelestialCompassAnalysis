95414957ef4f8eb7e699307356f1b69e0a795da5
Robert Mitchell <r.mitchell@ed.ac.uk>
2022-10-29 17:41:30 +0100

    Added a full plotting script.
    Can now produce offline plots of the same kind used in Sardinia.
    (The original scripts used to produce those plots were lost.)

--- templates/plot_json.py
+++ templates/plot_json.py
@@ -4,41 +4,68 @@ import numpy as np
 import json
 import argparse
 import os
+from scipy.stats import circmean
 
-def act(s):
+def act(s, log=True):
     """
-    Photoreceptor activation function.
+    Photoreceptor activation function. Gkanias et al. (2019) uses square
+    root activation, however log activation appears to generate a slightly
+    more stable angular representation. 
 
     :param s: The input to the photoreceptor.
+    :param log: Set True to use Log activation rather than square root.
     :return: The firing rate of the photoreceptor given input s.
     """
-    if s == 0:
-        return 0
-    else:
-        return np.log(s)
-#    return np.sqrt(s)
+    if not log:
+        return np.sqrt(s)
+
+    return np.log(s)
 
 def synchronise(d, zero=True, unwrap=True):
-    d = np.array([ x if x >= 0 else x + 2*np.pi for x in d])
+    """
+    Zero each angle series and adjust values so wraps plot nicely.
+
+    :param d: A series of angles (a single recording)
+    :param zero: Shift series so angles start from zero
+    :param unwrap: Unwrap angles which jump over 0/2pi boundary.
+    :returns: The adjusted array.
+    """
+    d = np.array(d)
     if zero:
         d = d - d[0]
+    d = d % (2*np.pi)
     if unwrap:
         for i in range(len(d) - 1):
-            if (d[i] < np.pi/4) and (d[i+1] > 7*np.pi/4):
+            if (d[i] < 3*np.pi/4) and (d[i+1] > 7*np.pi/4):
                 d[i+1] = d[i+1] - 2*np.pi
-            elif (d[i] > 7*np.pi/4) and (d[i+1] < np.pi/4):
+            elif (d[i] > 7*np.pi/4) and (d[i+1] < 3*np.pi/4):
                 d[i+1] = d[i+1] + 2*np.pi
 
     return d
 
 def decode_sky_compass(po,
                        n_sol=8,
-                       pol_prefs=np.radians([0,90,180,270,45,135,225,315]),
+                       pol_prefs=np.radians([0,90,180,270,45,135,225,315])
 ):
+    """
+    Sky compass decoding routine from Gkanias et al. (2019). This has been
+    implemeneted from scratch using the equations presented as a demonstration
+    that everything can be programmed from scratch without calling out to
+    external libraries or FFT implementations.
+
+    :param po: Pol-op unit responses
+    :param n_sol: The number of SOL neurons in use
+    :param pol_prefs: The preferred angles of each POL neuron. This is determined
+                      by the hardware configuration at the time the recording was
+                      taken.
+    :returns: A list of azimuthal angles with a list of associated confidence
+              in each.
+    """
+    # po structure
     # po[p] Unit p
     # po[p][t] readings for unit p at time t
     # po[p][t][d] reading for diode d of unit p at time t
-    units, duration, diodes = po.shape # Know the shape dimension in advance
+    units, duration, diodes = po.shape
 
     n_pol = len(pol_prefs)
     sol_prefs = np.linspace(0,2*np.pi - (2*np.pi / n_sol),n_sol)
@@ -48,10 +75,12 @@ def decode_sky_compass(po,
 
     for t in range(duration): # For each timestep
         # Get photodiode responses for each unit
-        unit_responses = [po[x][t] for x in range(units)]
-        ss = [ (s_h, s_v) for [s_v, s_h, _, _] in unit_responses ]
+        unit_responses = [ po[x][t] for x in range(units) ]
+        ss = [ (s_h, s_v) for [_, _, s_v, s_h] in unit_responses ]
         rates = [ (act(h), act(v)) for (h, v) in ss ]
         r_pol = [ (r_h - r_v) / (r_h + r_v) for (r_h, r_v) in rates ]
+
+        # Init for sum
         r_sol = np.zeros(n_sol)
         R = 0
         for z in range(n_sol):
@@ -64,6 +93,8 @@ def decode_sky_compass(po,
 
         a = np.real(R)
         b = np.imag(R)
+
+        # Compute argument and magnitude of complex conjugate of R (Eq. (4))
         angle = np.arctan2(-b,a)
         confidence = np.sqrt(a**2 + b**2)
         angular_outputs.append(angle)
@@ -71,22 +102,8 @@ def decode_sky_compass(po,
 
     return angular_outputs, confidence_outputs
 
-if __name__=="__main__":
-    parser = argparse.ArgumentParser(
-        description="Produce a recording-session plot from json."
-    )
 
-    parser.add_argument("-s", dest="session", type=str, required=True,
-                        help="The recording session directory."
-    )
-    parser.add_argument("-o", dest="output", type=str, required=False,
-                        help="Desired output file (specify matplotlib-supported format. PDF default."
-    )
-
-    args = parser.parse_args()
-
-    session = args.session
-    outfile = args.output
+def produce_plot(session, outfile=None):
 
     #
     # Pull in data
@@ -94,7 +111,7 @@ if __name__=="__main__":
     if not os.path.isdir(session):
         print("-s must specify a directory")
         sys.exit()
-
+    calling_dir = os.getcwd()
     os.chdir(session)
     imagefile = [x for x in os.listdir() if ".jpg" in x]
     recordings = [x for x in os.listdir() if ".json" in x]
@@ -115,19 +132,29 @@ if __name__=="__main__":
               ["image","imu_yaw"],
               ["image","averages"]]
 
-    fig, ax = plt.subplot_mosaic(mosaic)
+    fig, ax = plt.subplot_mosaic(mosaic, figsize=(10,4))
 
     #
     # Plot image if available
     #
     if imagefile != None:
         print(imagefile)
+        if outfile == None:
+            outfile = imagefile.split(".")[0] + ".pdf"
+
         img = mpimg.imread(imagefile)
         ax["image"].imshow(img)
-        ax["image"].set_title(imagefile.split(".")[0])
+        ax["image"].set_title(imagefile.split(".")[0].replace("_", " "))
     else:
+        if outfile == None:
+            outfile = "out.pdf"
         ax["image"].set_title("No image found")
 
+    max_length = 0
+    min_length = 0
+    first = True
+    corrected_skycompass_table = []
+    corrected_imu_table = []
     for r in recordings:
         with open(r) as f:
             data = json.load(f)
@@ -142,46 +169,82 @@ if __name__=="__main__":
         po = np.array(po)
         pol_sensor_angle = synchronise(np.array(decode_sky_compass(po)[0]))
 
+        if max_length < len(imu):
+            max_length = len(imu)
 
-        # # Shift data to start at 0
-        # offset = data["yaw"][0]
-        # data["yaw"] = data["yaw"] - offset
-        # pol_offset = pol_sensor_angle[0]
-        # pol_sensor_angle = pol_sensor_angle - pol_offset
+        if first:
+            first = False
+            min_length = len(imu)
 
-        # # Add 2pi to angles less than 0 to move into 0,360
-        # data["yaw"] = [ x if x >= 0 else x + 2*np.pi for x in data["yaw"]]
-        # pol_sensor_angle = [
-        #     x if x >= 0 else x + 2*np.pi for x in pol_sensor_angle
-        # ]
-
-        # # Check for jumps across 0/360 and correct for the sake of plotting
-        # for i in range(len(data["yaw"]) - 1):
-        #     # Jumps from >0 to <360
-        #     if data["yaw"][i] < np.pi/4 and data["yaw"][i + 1] > 7*np.pi/4:
-        #         data["yaw"][i + 1] = data["yaw"][i + 1] - 2*np.pi
-
-        #     # Jumps from <360 to >0
-        #     if data["yaw"][i] > 7*np.pi/4 and data["yaw"][i + 1] < np.pi/4:
-        #         data["yaw"][i + 1] = data["yaw"][i + 1] + 2*np.pi
-
-        #     # Jumps from >0 to <360
-        #     if pol_sensor_angle[i] < np.pi/4 and pol_sensor_angle[i + 1] > 7*np.pi/4:
-        #         pol_sensor_angle[i + 1] = pol_sensor_angle[i + 1] - 2*np.pi
-
-        #     # Jumps from <360 to >0
-        #     if pol_sensor_angle[i] > 7*np.pi/4 and pol_sensor_angle[i + 1] < np.pi/4:
-        #         pol_sensor_angle[i + 1] = pol_sensor_angle[i + 1] + 2*np.pi
-
-
-        ax["skycompass"].plot(pol_sensor_angle)
+        if min_length > len(imu):
+            min_length = len(imu)
 
         # Plot cleaned angles
+        ax["skycompass"].plot(pol_sensor_angle)
         ax["imu_yaw"].plot(imu)
+
+        corrected_skycompass_table.append(pol_sensor_angle)
+        corrected_imu_table.append(imu)
+
+    # Compute mean angle over all reacordings at each timestep.
+    sc_means = []
+    imu_means = []
+    for t in range(min_length):
+        sc_angles = [x[t] for x in corrected_skycompass_table]
+        imu_angles = [x[t] for x in corrected_imu_table]
+        scmean = circmean(sc_angles, nan_policy='omit')
+        imumean = circmean(imu_angles, nan_policy='omit')
+        sc_means.append(scmean)
+        imu_means.append(imumean)
+
+    # Plot (clean) averages
+    ax["averages"].plot(synchronise(sc_means))
+    ax["averages"].plot(synchronise(imu_means))
+
+    # Add text
+    # ax["skycompass"].text(0,5,"Sky Compass")
+    # ax["imu_yaw"].set_ylim([-padding, 2*np.pi + padding])
+    # ax["averages"].set_ylim([-padding, 2*np.pi + padding])
+
+    ax["skycompass"].set_title("Sky Compass")
+    ax["imu_yaw"].set_title("IMU (Yaw)")
+    ax["averages"].set_title("Circular average")
+    fig.tight_layout()
 
     # General formatting
     ax["image"].set_xticks([])
     ax["image"].set_yticks([])
+    padding = 0.7
+    ax["skycompass"].set_ylim([-padding, 2*np.pi + padding])
+    ax["imu_yaw"].set_ylim([-padding, 2*np.pi + padding])
+    ax["averages"].set_ylim([-padding, 2*np.pi + padding])
+    x_axis_padding = 20
+    ax["skycompass"].set_xlim([-x_axis_padding, max_length + x_axis_padding])
+    ax["imu_yaw"].set_xlim([-x_axis_padding, max_length + x_axis_padding])
+    ax["averages"].set_xlim([-x_axis_padding, max_length + x_axis_padding])
+    ax["skycompass"].set_xticks([])
+    ax["imu_yaw"].set_xticks([])
 
-    plt.show()
+    os.chdir(calling_dir)
+    fig.savefig(outfile, bbox_inches="tight")
+#    plt.show()
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(
+        description="Produce a recording-session plot from json."
+    )
+
+    parser.add_argument("-s", dest="session", type=str, required=True,
+                        help="The recording session directory."
+    )
+    parser.add_argument("-o", dest="output", type=str, required=False,
+                        help="Desired output file (specify matplotlib-supported format. PDF default."
+    )
+
+    args = parser.parse_args()
+
+    session = args.session
+    outfile = args.output
+
+    produce_plot(session, outfile=outfile)
 
